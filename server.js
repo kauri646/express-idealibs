3e5f1734919ccc89254bc133ffa3fd778b12f7c1
kautsar <kautsarrifqi1@gmail.com>
2024-02-23 01:19:39 +0700

    add welcome greet

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ app.use("/users", usersRoutes)
 app.use(express.json());
 app.use(cookieParser())
 
+app.get("/", (req, res)=> {
+    res.send("Welcome to Idealibs")
+})
 app.listen(port, () => console.log(`app listening on port ${port}`))
