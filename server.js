a339a7138947168a73b40ea6140b66c2dc83a13d
kautsar <kautsarrifqi1@gmail.com>
2024-03-22 00:14:42 +0700

    remove oauth

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ import express from 'express';
 import usersRouter from './src/routes/user.js';
 import filesRouter from './src/routes/file.js';
 import cookieParser from 'cookie-parser';
-import { OAuth2Client } from 'google-auth-library';
 import { config } from "dotenv";
 import fileUpload from "express-fileupload"
 import cors from 'cors'; // Import middleware CORS
-import { saveUserToDatabase } from './src/config/supabase.js'; 
 config({ path: '.env' });
 
 const app = express();
@@ -20,46 +18,6 @@ app.use(fileUpload())
 app.use("/users", usersRouter);
 app.use("/files", filesRouter);
 
-const client = new OAuth2Client({
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  redirectUri: process.env.GOOGLE_REDIRECT_URI
-});
-
-app.get('/auth/google', (req, res) => {
-  const url = client.generateAuthUrl({
-    access_type: 'offline',
-    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
-  });
-  res.redirect(url);
-});
-
-app.get('/auth/google/callback', async (req, res) => {
-  const { code } = req.query;
-  try {
-    const { tokens } = await client.getToken(code);
-    const user = await getUserInfo(tokens.access_token);
-    // Simpan informasi pengguna ke database
-    await saveUserToDatabase(user);
-    // Redirect pengguna ke halaman utama
-    res.redirect('/');
-  } catch (error) {
-    console.error('Error retrieving tokens:', error);
-    res.status(500).send('Authentication failed');
-  }
-});
-
 app.listen(port, () => {
   console.log(`Server berjalan pada http://localhost:${port}`);
 });
-
-async function getUserInfo(accessToken) {
-  const { data } = await client.request({
-    url: 'https://www.googleapis.com/oauth2/v2/userinfo',
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`
-    }
-  });
-  return data;
-}
